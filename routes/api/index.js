1ea216affb82fb168ef8332cc77c2cb123c3b1bf
arellanoe <112835221+arellanoe@users.noreply.github.com>
2023-03-09 13:44:10 -0700

    Update index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,15 +1,10 @@
-// Set requirements (Express Router)
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
-// Set routes (user and thought routes)
-const usersRoutes = require('./user-routes');
-const thoughtsRoutes = require('./thought-routes');
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
-// Add `/users` to created routes 
-router.use('/users', usersRoutes);
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
-// Add `/thoughts` to created routes 
-router.use('/thoughts', thoughtsRoutes);
-
-// Export Module Router
 module.exports = router;
